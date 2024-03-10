336122619d42c11af8cd99544a68daf643f3f467
NetGoatFr <132133277+netgoatfr@users.noreply.github.com>
2024-03-10 10:11:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-setup(name='logger',
+setup(name='colorful_logger',
     version='1.0',
     description='Colorful and useful logging utility module for python',
     author='netgoatfr',
     author_email='netgoatfr@gmail.com',
     license='CC BY-SA',
-    packages=['logger'],
     install_requires=[
         "datetime",
         "colorama",
